8842110428c8db7d46082bd038e14c637827c0a9
Carlos Quijano <carlos.quijano@gmail.com>
2019-02-26 18:44:48 -0600

    Add test framework sample spec files

---
+++ e2e/pages/webdriverhub/webdriverhub.home.page.ts
@@ -0,0 +1,28 @@
+import {browser, element, by} from 'protractor';
+
+export class WebDriverHubHomePage {
+
+    private readonly pageUrl = 'http://localhost:4444/wd/hub/static/resource/hub.html';
+
+    /**
+     * Gets the page from the `browser` object.
+     */
+    get() {
+        browser.waitForAngularEnabled(false);
+        browser.driver.get(this.pageUrl);
+    }
+
+    /**
+     * Gets the page title from the `browser` object.
+     */
+    getPageTitle() {
+        return browser.driver.getTitle();
+    }
+
+    /**
+     * Gets the server info banner element.
+     */
+    getServerInfo(): any {
+        return element(by.css('.server-info'));
+    }
+}
---
+++ e2e/specs/suites.ts
@@ -0,0 +1,7 @@
+export enum TestSuites {
+    healthCheckSuite = 'HealthCheck Tests',
+    smokeSuite = 'Smoke Tests',
+    regressionSuite = 'Regression Tests',
+    e2eSuite = 'End to End Test Suite',
+    frameworkSuite = 'Test Framework Suite'
+}
