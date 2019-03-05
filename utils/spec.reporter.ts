a9a39486a99c3e57549de8d3c34110956ac8f3d4
Carlos Quijano <carlos.quijano@gmail.com>
2019-03-05 15:53:30 -0600

    Refactor project name

--- utils/spec.reporter.ts
+++ utils/spec.reporter.ts
@@ -1,16 +1,12 @@
 import { DisplayProcessor, SpecReporter } from 'jasmine-spec-reporter';
 import SuiteInfo = jasmine.SuiteInfo;
 
-
-class SpecDisplayProcessor extends DisplayProcessor {
-    public displayJasmineStarted(info: SuiteInfo, log: string): string {
-        return `TypeScript ${log}`;
-    }
-}
-
 export let commonSpecReporter = new SpecReporter({
     spec: {
-        displayStacktrace: true
-    },
-    customProcessors: [SpecDisplayProcessor]
+        displayStacktrace : true,
+        displayFailed : true,
+        displaySuccessful : true,
+        displayPending : true,
+        displayDuration : true
+    }
 });
